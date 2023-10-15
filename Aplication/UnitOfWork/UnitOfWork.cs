3ec592276aa2d8d1017f1b0d6d5dccee149618ef
Jhosep <jhoseppulido1129@gmail.com>
2023-10-15 13:47:29 -0500

    feat(Aplication): :sparkles: All Repositories Linked To UnitOfWork

--- Aplication/UnitOfWork/UnitOfWork.cs
+++ Aplication/UnitOfWork/UnitOfWork.cs
@@ -15,10 +15,7 @@ public class UnitOfWork : IUnitOfWork, IDisposable
     {
         get
         {
-            if (_roles == null)
-            {
-                _roles = new RolRepository(_context);
-            }
+            _roles ??= new RolRepository(_context);
             return _roles;
         }
     }
@@ -27,13 +24,98 @@ public class UnitOfWork : IUnitOfWork, IDisposable
     {
         get
         {
-            if (_users == null)
-            {
-                _users = new UserRepository(_context);
-            }
+            _users ??= new UserRepository(_context);
             return _users;
         }
     }
+        private IVeterinarian _Veterinarian;
+    public IVeterinarian Veterinarian
+    {
+        get { 
+            _Veterinarian ??= new VeterinarianRepository(_context);
+                return _Veterinarian;
+            }
+    }
+    private ISupplier _Supplier;
+    public ISupplier Supplier
+    {
+        get { 
+            _Supplier ??= new SupplierRepository(_context);
+                return _Supplier;
+            }
+    }
+    private ISpecies _Species;
+    public ISpecies Species
+    {
+        get { 
+            _Species ??= new SpeciesRepository(_context);
+                return _Species;
+            }
+    }
+    private ISoldMedicine _SoldMedicine;
+    public ISoldMedicine SoldMedicine
+    {
+        get { 
+            _SoldMedicine ??= new SoldMedicineRepository(_context);
+                return _SoldMedicine;
+            }
+    }
+    private IPurchasedMedicine _PurchasedMedicine;
+    public IPurchasedMedicine PurchasedMedicine
+    {
+        get { 
+            _PurchasedMedicine ??= new PurchasedMedicineRepository(_context);
+                return _PurchasedMedicine;
+            }
+    }
+    private IPet _Pet;
+    public IPet Pet
+    {
+        get { 
+            _Pet ??= new PetRepository(_context);
+                return _Pet;
+            }
+    }
+    private IOwner _Owner;
+    public IOwner Owner
+    {
+        get { 
+            _Owner ??= new OwnerRepository(_context);
+                return _Owner;
+            }
+    }
+    private IMedicine _Medicine;
+    public IMedicine Medicine
+    {
+        get { 
+            _Medicine ??= new MedicineRepository(_context);
+                return _Medicine;
+            }
+    }
+    private IMedicalTreatment _MedicalTreatment;
+    public IMedicalTreatment MedicalTreatment
+    {
+        get { 
+            _MedicalTreatment ??= new MedicalTreatmentRepository(_context);
+                return _MedicalTreatment;
+            }
+    }
+    private IAppointment _Appointment;
+    public IAppointment Appointment
+    {
+        get { 
+            _Appointment ??= new AppointmentRepository(_context);
+                return _Appointment;
+            }
+    }
+    private IBreed _Breed;
+    public IBreed Breed
+    {
+        get { 
+            _Breed ??= new BreedRepository(_context);
+                return _Breed;
+            }
+    } 
     public async Task<int> SaveAsync()
     {
         return await _context.SaveChangesAsync();
