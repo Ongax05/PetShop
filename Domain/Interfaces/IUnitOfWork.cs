f1395b646d3b14dc845e0c36220dc19dbc20244c
Jhosep <jhoseppulido1129@gmail.com>
2023-10-15 13:23:10 -0500

    feat(Domain): :sparkles: All Interfaces linked to IUnitOfWork

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -4,5 +4,16 @@ public interface IUnitOfWork
 {
     IRolRepository Roles { get; }
     IUserRepository Users { get; }
+    IAppointment Appointment { get; }
+    IBreed Breed { get; }
+    IMedicalTreatment MedicalTreatment { get; }
+    IMedicine Medicine { get; }
+    IOwner Owner { get; }
+    IPet Pet { get; }
+    IPurchasedMedicine PurchasedMedicine { get; }
+    ISoldMedicine SoldMedicine { get; }
+    ISpecies Species { get; }
+    ISupplier Supplier { get; }
+    IVeterinarian Veterinarian { get; }
     Task<int> SaveAsync();
 }
